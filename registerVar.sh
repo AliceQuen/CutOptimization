19357e5c117106417e8d5960e8854109d971002b
Alice_Quen <qjk21@mails.tsinghua.edu.cn>
2024-06-03 14:40:57 +0800

    On branch main
     Changes to be committed:
    	modified:   registerVar.sh
    
    	Changed realization of registerVar.h
    	Now it is configured with a VarList.out and will create a more organized directory
    	tree

--- registerVar.sh
+++ registerVar.sh
@@ -1,157 +1,149 @@
 #!/bin/bash
 # Please first run this script to generate files for cut optimization
-# Substitute variables below to what you want 
 
-# Task information
-taskName='cutOpt7'
-dataDir="/home/qjk/JPsiX3872/Cut9/"
-
+############################### Configuration #############################
+taskName='cutOpt7' # Name of this task
+jobScriptDir='/home/qjk/JPsiX3872/CutOptimization/JobScripts' # Job script Directory, DO NOT INCLUDE last '/', force to re-create a jobScriptDir/taskName 
+dataDir="/home/qjk/JPsiX3872/Cut9" # Data file Directory, DO NOT INCLUDE last '/'
+var_OUT='VarList.out' # VarList.out file
+Mode='1' # Mode of running, 0:Generate VarList.out File templete, 1:Generate job scripts
+Discription=' Test job' # Job discription, will be saved in jobScriptDir as .md file
+###########################################################################
 # Variables
-declare -a varName 
-declare -a varMax # Range for primary cut
-declare -a varMin 
-declare -a varMaxOpt # Range for cut to vary
-declare -a varMinOpt
-declare -a varType # Variable for deciding varying upper bundary or lower bundary. 0 for upper and 1 for lower
 
-varName=(jpsi2_eta_abs jpsi2_massErr mu3_eta_abs mu4_eta_abs jpsi2_psi2s_dR)
-varMax=(10000 10000 10000 10000 10000)
-varMin=(-10000 -10000 -10000 -10000 -10000)
-varMaxOpt=(2.7 0.068 0 0 5.5)
-varMinOpt=(2.1 0.04 0 0 3.1)
-varType=(0 0 0 0 0) # mu eta is not used but have to be included for correctly loading data, please remove them in Execute.C
-
-################## Below is the functional part! Be careful when modifying it! ##################
-
-# First judge weather vectors above have the same demention
-
-size1=0;size2=0;size3=0;size4=0;size5=0;size6=0
-
-for i in ${varName[@]}
-do
-	size1=$(expr $size1 + 1)
-done
-
-for i in ${varMin[@]}
-do
-	size2=$(expr $size2 + 1)
-done
-
-for i in ${varMax[@]}
-do
-	size3=$(expr $size3 + 1)
-done
-
-for i in ${varType[@]}
-do
-	size4=$(expr $size4 + 1)
-done
-
-for i in ${varMaxOpt[@]}
-do
-        size5=$(expr $size5 + 1)
-done
-
-for i in ${varMinOpt[@]}
-do
-        size6=$(expr $size6 + 1)
-done
+############### Below is the functional part! Be careful when modifying it! ################
+if [ $Mode -ne 1 ]
+then
+	echo -e "--- Description ---\nVarName/CutMin/CutMax/OptMax/OptMin/Mode\n" > VarList.out
+	# CutMin/Max is pre-cut range and OptMin/Max is cut option varying range 
+	# Mode = 0 for varying Maximum and 1 for varying Minimun
+	return
+fi
+jobScriptDir=${jobScriptDir}/${taskName}
+rm -rf ${jobScriptDir}
+mkdir -p ${jobScriptDir}
+echo -e "${Discription}\nConfiguration\n${taskName}\njobScriptDir\ndataDir\nvar_OUT" > ${jobScriptDir}/${taskName}_README.md
 # Find all data files
 declare -a dataFile
-dataFile=$dataDir*
+dataFile="${dataDir}/*"
 fileNumber=0
 for i in ${dataFile[@]}
 do
 	fileNumber=$(expr $fileNumber + 1);
 done
 
-if [[ $size1 -eq $size2 && $size1 -eq $size3 && $size1 -eq $size4 && $size1 -eq $size5 && $size1 -eq $size6 && $fileNumber -ne 0 ]] ; then
-	# register in var.h
-	text=''
-	cat var.h > var_$taskName.h
-	for (( i=0; i<$size1; i=i+1))
-	do
-		text="\nRooRealVar ${varName[$i]}(\"${varName[$i]}\", \"${varName[$i]}\", ${varMin[$i]}, ${varMax[$i]});"
-		sed -i -e '$a\'"${text}" var_$taskName.h
-		sed -i -e '$a\'"const double ${varName[$i]}_Min = ${varMin[$i]};" var_$taskName.h
-		sed -i -e '$a\'"const double ${varName[$i]}_Max = ${varMax[$i]};" var_$taskName.h
-		sed -i -e '$a\'"const double ${varName[$i]}_MaxOpt = ${varMaxOpt[$i]};" var_$taskName.h
-		sed -i -e '$a\'"const double ${varName[$i]}_MinOpt = ${varMinOpt[$i]};" var_$taskName.h
-	done
-
-	#register in function.h
-	text=''
-	sed -e 's/var.h/'"var_$taskName.h"'/' function.h > function_$taskName.h
-	for ((i=0; i<$size1; i=i+1))
-	do
-		sed -i -e '/Add_Other_Variables_Here/i\'"    variables.add(${varName[$i]});" function_$taskName.h
-	done
-	sed -i -e '/Add_Other_Variables_Here/d' function_$taskName.h
-	# register all .C files
-	for (( i=0; i<$size1; i=i+1))
-	do
-		if [ ${varType[$i]} -eq 0 ]
-                then
-                        sed -e '3s/function.h/'"function_$taskName.h/" ParaOptMax.C > ${varName[$i]}_$taskName.C
-                else
-                        sed -e '3s/function.h/'"function_$taskName.h/" ParaOptMin.C > ${varName[$i]}_$taskName.C
-                fi
-		sed -i -e '1,$s/VarName/'"${varName[$i]}/g" ${varName[$i]}_$taskName.C
-		sed -i -e 's/TaskName/'"${taskName}/" ${varName[$i]}_$taskName.C
-	done
-	# register Execute files
-	sed -e 's/Execute/'"Execute_${taskName}/" Execute.C > Execute_${taskName}.C
-	sed -i -e '/Register/a\REGISTER' Execute_${taskName}.C
-	for (( i=0; i<$size1; i=i+1))
-	do
-		sed -i -e '/REGISTER/i\'"        name->push_back(\"${varName[$i]}_$taskName.C\");" Execute_${taskName}.C
-	done
-	sed -i -e '/REGISTER/d' Execute_${taskName}.C
-	sed -i -e 's/function.h/'"function_${taskName}.h/"  Execute_${taskName}.C
-	# register data files
-	text=''
-	num=$(expr $fileNumber - 1)
-	for i in ${dataFile[@]}
-	do
-		text="${text}$(echo $i | sed -e 's:'"$dataDir::g"), "
-	done
-	text=${text%, } # Delete last , 
-	for (( i=0; i<$size1; i=i+1))
-	do
-		sed -i -e '1,/Data_List/{s:Data_List:'"${text}:}"  ${varName[$i]}_$taskName.C 
-        done
-	# register DrawCut.C
-	text=''
-        num=$(expr $fileNumber - 1)
-        for i in ${dataFile[@]}
-        do
-                text="${text}$(echo $i | sed -e 's:'"$dataDir::g"), "
-        done
-        text=${text%, } # Delete last ,
-        sed -e '1,/Data_List/{s:Data_List:'"${text}:}" DrawCut.C > DrawCut_$taskName.C
-	sed -i -e 's:DataDir:'"${dataDir}:" DrawCut_$taskName.C
-	sed -i -e 's:DrawCut:'"DrawCut_${taskName}:" DrawCut_$taskName.C
-	# register result and data dir 
-	mkdir -p result/${taskName}_result
-	for (( i=0; i<$size1; i=i+1))
-        do
-		sed -i -e 's:DataDir:'"${dataDir}:"  ${varName[$i]}_$taskName.C
-	done
-else
-	if [[ $fileNumber -eq 0 ]]
+# register in var.h
+text=''
+cat var.h > ${jobScriptDir}/var_$taskName.h
+cat ${var_OUT} | while read rows
+do
+	if [[ $rows =~ ---.*--- ]]
 	then
-		echo 'No data files'
-	fi
-	if [[ $size1 -eq 0 || $size2 -eq 0 || $size3 -eq 0 || $size4 -eq 0 || $size5 -eq 0 || $size6 -eq 0 ]]
-	then
-		echo 'Some var vectors have 0 demention'
-	fi
-	if [[ $size1 -eq $size2 && $size1 -eq $size3 && $size1 -eq $size4 && $size1 -eq $size5 && $size1 -eq $size6 ]]
-	then
-		echo 'Please check'
+		sleep 0.001
 	else
-		echo 'Var vectors have different dementions!'
-		echo "size1 $size1 size2 $size2 size3 $size3 size4 $size4 size5 $size5 size6 $size6"
-		echo 'Please check'
+		varName=$(echo $rows | awk 'BEGIN{FS="/"} {print $1}')
+		varMin=$(echo $rows | awk 'BEGIN{FS="/"} {print $2}')
+		varMax=$(echo $rows | awk 'BEGIN{FS="/"} {print $3}')
+		varMinOpt=$(echo $rows | awk 'BEGIN{FS="/"} {print $4}')
+		varMaxOpt=$(echo $rows | awk 'BEGIN{FS="/"} {print $5}')
+		text="\nRooRealVar ${varName}(\"${varName}\", \"${varName}\", ${varMin}, ${varMax});"
+		sed -i -e '$a\'"${text}" ${jobScriptDir}/var_$taskName.h
+		sed -i -e '$a\'"const double ${varName}_Min = ${varMin};" ${jobScriptDir}/var_$taskName.h
+		sed -i -e '$a\'"const double ${varName}_Max = ${varMax};" ${jobScriptDir}/var_$taskName.h
+		sed -i -e '$a\'"const double ${varName}_MaxOpt = ${varMaxOpt};" ${jobScriptDir}/var_$taskName.h
+		sed -i -e '$a\'"const double ${varName}_MinOpt = ${varMinOpt};" ${jobScriptDir}/var_$taskName.h
 	fi
-fi
+done
+#register in function.h
+text=''
+sed -e 's:var.h:'"var_${taskName}.h:g" function.h > ${jobScriptDir}/function_$taskName.h
+cat ${var_OUT} | while read rows
+do
+	if [[ $rows =~ ---.*--- ]]
+	then
+		sleep 0.001
+	else
+		varName=$(echo $rows | awk 'BEGIN{FS="/"} {print $1}')
+		sed -i -e '/Add_Other_Variables_Here/i\'"    variables.add(${varName});" ${jobScriptDir}/function_$taskName.h
+	fi
+done
+sed -i -e '/Add_Other_Variables_Here/d' ${jobScriptDir}/function_$taskName.h
+# register all .C files
+cat ${var_OUT} | while read rows
+do
+	if [[ $rows =~ ---.*--- ]]
+	then
+		sleep 0.001
+	else
+		varName=$(echo $rows | awk 'BEGIN{FS="/"} {print $1}')
+       		varMin=$(echo $rows | awk 'BEGIN{FS="/"} {print $2}')
+    		varMax=$(echo $rows | awk 'BEGIN{FS="/"} {print $3}')
+		varMimOpt=$(echo $rows | awk 'BEGIN{FS="/"} {print $4}')
+	   	varMaxOpt=$(echo $rows | awk 'BEGIN{FS="/"} {print $5}')
+		varType=$(echo $rows | awk 'BEGIN{FS="/"} {print $6}')
+		if [[ ${varType} -eq 0 ]]
+		then
+			sed -e '3s/function.h/'"function_$taskName.h/" ParaOptMax.C > ${jobScriptDir}/${varName}_$taskName.C
+		else
+			sed -e '3s/function.h/'"function_$taskName.h/" ParaOptMin.C > ${jobScriptDir}/${varName}_$taskName.C
+		fi
+		sed -i -e '1,$s/VarName/'"${varName}/g" ${jobScriptDir}/${varName}_$taskName.C
+		sed -i -e 's/TaskName/'"${taskName}/" ${jobScriptDir}/${varName}_$taskName.C
+	fi
+done
+# register Execute files
+sed -e 's/Execute/'"Execute_${taskName}/" Execute.C > ${jobScriptDir}/Execute_${taskName}.C
+sed -i -e '/Register/a\REGISTER' ${jobScriptDir}/Execute_${taskName}.C
+cat ${var_OUT} | while read rows
+do
+	if [[ $rows =~ ---.*--- ]]
+	then
+		sleep 0.001
+	else
+		varName=$(echo $rows | awk 'BEGIN{FS="/"} {print $1}')
+		sed -i -e '/REGISTER/i\'"        name->push_back(\"${varName}_$taskName.C\");" ${jobScriptDir}/Execute_${taskName}.C
+	fi
+done
+sed -i -e '/REGISTER/d' ${jobScriptDir}/Execute_${taskName}.C
+sed -i -e 's/function.h/'"function_${taskName}.h/"  ${jobScriptDir}/Execute_${taskName}.C
+# register data files
+text=''
+num=$(expr $fileNumber - 1)
+for i in ${dataFile[@]}
+do
+	text="${text}$(echo $i | sed -e 's:'"${dataDir}/::g"), "
+done
+text=${text%, } # Delete last , 
+cat ${var_OUT} | while read rows
+do
+	if [[ $rows =~ ---.*--- ]]
+	then 
+		sleep 0.001
+	else
+		varName=$(echo $rows | awk 'BEGIN{FS="/"} {print $1}')
+		sed -i -e '1,/Data_List/{s:Data_List:'"${text}:}"  ${jobScriptDir}/${varName}_$taskName.C 
+	fi
+done
+# register DrawCut.C
+text=''
+num=$(expr $fileNumber - 1)
+for i in ${dataFile[@]}
+do
+	text="${text}$(echo $i | sed -e 's:'"${dataDir}/::g"), "
+done
+text=${text%, } # Delete last ,
+sed -e '1,/Data_List/{s:Data_List:'"${text}:}" DrawCut.C > ${jobScriptDir}/DrawCut_$taskName.C
+sed -i -e 's:DataDir:'"${dataDir}/:" ${jobScriptDir}/DrawCut_$taskName.C
+sed -i -e 's:DrawCut:'"DrawCut_${taskName}:" ${jobScriptDir}/DrawCut_$taskName.C
+# register result and data dir 
+mkdir -p ${jobScriptDir}/result/${taskName}_result
+cat ${var_OUT} | while read rows
+do
+	if [[ $rows =~ ---.*--- ]]
+	then
+		sleep 0.001
+	else
+		varName=$(echo $rows | awk 'BEGIN{FS="/"} {print $1}')
+		sed -i -e 's:DataDir:'"${dataDir}/:"  ${jobScriptDir}/${varName}_$taskName.C
+	fi
+done
